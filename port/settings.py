942a34f6c41e1640f6d133e5d7677c5b13d7fc6e
Buchi77344 <onyebuchifestus77@gmail.com>
2025-04-01 12:02:28 +0100

    asfldkfem

--- port/settings.py
+++ port/settings.py
@@ -121,6 +121,8 @@ STATIC_DIR = [
     os.path.join(BASE_DIR,'static')
 ]
 
+STATIC_ROOT = os.path.join(BASE_DIR,'static')
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
